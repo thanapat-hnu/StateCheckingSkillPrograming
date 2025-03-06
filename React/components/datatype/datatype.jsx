a81f48993789bee1e802957885c6e3c242db21dd
thanapathnu <thanapat.hnu@spumail.net>
2025-03-06 22:17:52 +0700

    update datatype.jsx

--- React/components/datatype/datatype.jsx
+++ React/components/datatype/datatype.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 const Datatype = () => {
   const str = "string"; // ข้อความ
   const int = 1; // จํานวนเต็ม
@@ -7,17 +5,29 @@ const Datatype = () => {
 
   const str999 = "999.99";
 
-  useEffect(() => {
-    console.log(typeof str + "  : " + str);
-    console.log(typeof int + "  : " + int);
-    console.log(typeof boolean + " : " + boolean);
-    console.log("--------------------");
-    console.log("str999" + " : " + str999 + " : " + typeof str999);
-    console.log(parseInt(str999) + " : " + typeof parseInt(str999));
-    console.log(parseFloat(str999) + " : " + typeof parseFloat(str999));
-    console.log("=====================");
-
-  }, []);
-  return <div></div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log(typeof str + "  : " + str);
+          console.log(typeof int + "  : " + int);
+          console.log(typeof boolean + " : " + boolean);
+          console.log("--------------------");
+        }}
+      >
+        แสดงตัวแปร
+      </button>
+      <button
+        onClick={() => {
+          console.log("str999" + " : " + str999 + " : " + typeof str999);
+          console.log(parseInt(str999) + " : " + typeof parseInt(str999));
+          console.log(parseFloat(str999) + " : " + typeof parseFloat(str999));
+          console.log("=====================");
+        }}
+      >
+        ทดสอบแปลง
+      </button>
+    </div>
+  );
 };
 export default Datatype;
