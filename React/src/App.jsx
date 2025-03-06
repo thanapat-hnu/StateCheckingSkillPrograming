2c3585abc2a172e5fd756361c99bb59ecd189afd
thanapathnu <thanapat.hnu@spumail.net>
2025-03-06 21:43:23 +0700

    add datatype.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Datatype from "../components/datatype/datatype.jsx";
 
 function App() {
-
-  return <div>
-    <h1>Hello World Web App</h1>
-  </div>;
+  return (
+    <div className="App">
+      <div className="hello">
+        <h1>Hello World Web App</h1>
+      </div>
+      <div className="datatype">
+        <Datatype />
+      </div>
+    </div>
+  );
 }
-
 export default App;
